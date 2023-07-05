b24af34016dede7b1b9ce5c4dbe1d33db7c799aa
Elinmaculado <miguel.ruiz@uniat.edu.mx>
2023-07-05 06:50:33 -0600

    level1

--- Assets/Scripts/Character/GGC_Scripts/PlayerMovement.cs
+++ Assets/Scripts/Character/GGC_Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    public float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
